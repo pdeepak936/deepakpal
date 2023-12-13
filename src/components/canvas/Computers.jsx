7a43567f49975819649e2f4d459bbe6efdfeac65
pdeepak936 <67180496+pdeepak936@users.noreply.github.com>
2023-12-13 11:18:06 +0530

    Price and Pool

---
+++ src/components/canvas/Computers.jsx
@@ -0,0 +1,16 @@
+import { Suspense, useEffect, useState} from 'react'
+import { Canvas } from '@react-three/fiber'
+import { OrbitControls, Preload, useGLTF } from '@react-three/drei'
+
+const Computers = () => {
+  const computer = useGLTF('./desktop_pc/scene.gltf')
+  return (
+    <mesh>
+      <hemisphereLight intensity={0.15} groundColor="black" />
+      <pintLight intensity={1} />
+      <primitive object={computer.scene} />
+    </mesh>
+  )
+}
+
+export default Computers
